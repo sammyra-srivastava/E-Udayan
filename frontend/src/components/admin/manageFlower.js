976f8ffc9de3c1688edfc01c741d97d6be85177e
sammyra_srivastava <srivastavkumkum20@gmail.com>
2022-04-26 18:19:56 +0530

    ..

--- frontend/src/components/admin/manageFlower.js
+++ frontend/src/components/admin/manageFlower.js
@@ -1,6 +1,17 @@
 import React from 'react'
+import { useState } from 'react';
 
 const ManageFlower = () => {
+  const [Flowers, setFlowers] = useState([
+    {
+      name:"",
+      color:"",
+      size:"",
+      
+    },
+
+  ]);
+  
   return (
     <div>manageFlower</div>
   )
--- frontend/src/components/main/header.js
+++ frontend/src/components/main/header.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
 
-const Header = () => {
-  return (
-    <div>header</div>
-  )
-}
-export default Header;
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
