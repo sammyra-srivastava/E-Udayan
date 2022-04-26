6ab9cfd2c1a6950a43a62b3510bfb11e33307ba9
sammyra_srivastava <srivastavkumkum20@gmail.com>
2022-04-26 18:33:54 +0530

    ..

--- frontend/src/components/main/header.js
+++ frontend/src/components/main/header.js
@@ -12,7 +12,7 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 
-const pages = ['Products', 'Pricing', 'Blog'];
+const pages = ['flower', 'florist', 'colors'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const Header = () => {
