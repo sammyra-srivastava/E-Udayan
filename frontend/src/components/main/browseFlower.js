068ce27a4114a01389038f375ebaf5a0de382259
m-triple-m <triplem656@gmail.com>
2022-04-28 17:56:46 +0530

    ..

--- frontend/src/components/main/browseFlower.js
+++ frontend/src/components/main/browseFlower.js
@@ -1,48 +1,11 @@
-import React from "react";
-import './App.css';
+import "./App.css";
 
+const BrowseFlower = () => {
+  return (
+    <div>
+      <h1>BrowseFlower</h1>
+    </div>
+  );
+};
 
-class BrowseFlower extends React.Component {
-   
-    // Constructor 
-    constructor(props) {
-        super(props);
-   
-        this.state = {
-            items: [],
-            DataisLoaded: false
-        };
-    }
-   
-    componentDidMount() {
-        fetch( "/florist/getall")
-            .then((res) => res.json())
-            .then((json) => {
-                this.setState({
-                    items: json,
-                    DataisLoaded: true
-                });
-            })
-    }
-    render() {
-        const { DataisLoaded, items } = this.state;
-        if (!DataisLoaded) return <div>
-            <h1> Pleses wait some time.... </h1> </div> ;
-   
-        return (
-        <div className = "App">
-            <h1> Fetch data from an api in react </h1>  {
-                items.map((item) => ( 
-                <ol key = { item.id } >
-                   Shop_Name: { item.shopName }, 
-                    Flowers: { item.flowers}, 
-                    Timings: { item.timings } 
-                    </ol>
-                ))
-            }
-        </div>
-    );
-}
-}
-   
 export default BrowseFlower;
