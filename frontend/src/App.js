1aa209c47f00ab48c86b43de232d04926cfaace5
sammyra_srivastava <srivastavkumkum20@gmail.com>
2022-06-21 17:24:47 +0530

    ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -64,6 +64,7 @@ function App() {
               path="browseflorist/:flowername"
             />
             <Route element={<FloristList />} path="floristlist" />
+            <Route element={<FloristList />} path="floristlist" />
             <Route element={<BrowseByFlorist />} path="browsebyflorist/:id" />
 
             <Route element={<ResetPassword />} path="resetpassword" />
